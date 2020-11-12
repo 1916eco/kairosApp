6e03769e31dbc38c8ff274d312dadd8a2318a19c
1916eco <1916eco@gmail.com>
2020-11-12 19:31:41 +0000

    api added with displaying default search

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import {  HttpClient } from '@angular/common/http';
+import {  Injectable } from '@angular/core';
+import{Observable} from 'rxjs'
+import {map} from 'rxjs/operators'
 
 @Component({
   selector: 'app-tab1',
@@ -7,8 +11,9 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  recipes = []
   constructor(private nav:NavController) {}
+
 
   slideOpts = {
     slidesPerView: 2,
@@ -17,9 +22,23 @@ export class Tab1Page {
   };
   clickedSearch(){
     console.log("Clicked Search")
+    this.getRecipes()
   }
   pushPage(){
     this.nav.navigateForward('/tab3.page')
+  }
+  async getRecipes() {
+    var appId = "66e70773"
+    var appKey = "cc03af7094cde3a4ce6cf1b91e277f6c"
+    // return this.http.get(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`)
+    // .pipe(
+    //   map(results => results['Search'])
+    // );
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`);
+    const data = await response.json();
+    this.recipes = data.hits;
+    console.log(data.hits);
+    
   }
 }
 
